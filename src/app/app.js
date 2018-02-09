f91774732d821c40100f0f891b142bb18b831c63
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2018-02-09 10:24:16 +0000

    Sanitise the npm script naming
    We thought the script names were a bit non-standard. This makes it
    more familer with other node projects and takes advantage of the
    standard `start` `test` scripts that don't require 'run'

--- src/app/app.js
+++ src/app/app.js
@@ -1,57 +1,19 @@
 import express from 'express';
-import pinoMiddleware from 'express-pino-logger';
 import helmet from 'helmet';
 import home from '../home';
 import orgs from '../orgs';
 import {pageNotFoundMiddleware, internalServerErrorMiddleware} from '../errors';
+import {loggerMiddleware} from '../logger';
+import csp from './app.csp';
 
 const app = express();
 
-app.use(pinoMiddleware());
+app.use(loggerMiddleware({}));
 
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: [
-      `'none'`
-    ],
-    styleSrc: [
-      `'self'`,
-      `'unsafe-inline'`
-    ],
-    scriptSrc: [
-      `'self'`,
-      'www.google-analytics.com',
-      `'sha256-+6WnXIl4mbFTCARd8N3COQmT3bJJmo32N8q8ZSQAIcU='`, // Inline script tag in govuk_template
-      `'sha256-G29/qSW/JHHANtFhlrZVDZW1HOkCDRc78ggbqwwIJ2g='`  // Inline script tag in govuk_template
-    ],
-    imgSrc: [
-      `'self'`,
-      'www.google-analytics.com'
-    ],
-    connectSrc: [
-      `'self'`,
-      'www.google-analytics.com'
-    ],
-    frameSrc: [
-      `'self'`
-    ],
-    fontSrc: [
-      `'self'`,
-      'data:'
-    ],
-    objectSrc: [
-      `'self'`
-    ],
-    mediaSrc: [
-      `'self'`
-    ]
-  }
-}));
+app.use(helmet.contentSecurityPolicy(csp));
 
-app.use('/assets', express.static('dist/assets', {
-  immutable: process.env.NODE_ENV === 'production'
-}));
+app.use('/assets', express.static('dist/assets', {immutable: true}));
 
 app.use('/orgs', orgs);
 app.use('/', home);
