b55d94e21152a401a8ebfe75867193fbfc7f0a74
chrisfarms <chris.farmiloe@digital.cabinet-office.gov.uk>
2018-02-12 21:06:05 +0000

    `npm run watch` incremental build/test runner
    Running `npm test` or `npm test:unit` is quite slow because it executes
    a full build.
    
    This implements running the tests (ENABLE_TESTS=true) via an onBuildExit
    hook so that they are run immeditely after an incremental build
    (ENABLE_WATCH=true).
    
    An `npm run watch` script is added to easily watch the tests.
    
    `ENABLE_TESTS=true npm start` would enable running the tests at the same
    time as the hot-reloading server, but the output would be a bit messy so
    probably best done seperately.
    
    A new flag (ENABLE_COVERAGE=true) is added to control istambul/nyc
    coverage so that the instrementation step can be skipped when not
    needed.

--- config/tests.config.js
+++ config/tests.config.js
@@ -1,27 +1,45 @@
 const path = require('path');
 const glob = require('glob');
+const WebpackShellPlugin = require('webpack-shell-plugin');
 
 module.exports = cfg => {
+  const onFail = process.env.ENABLE_WATCH === 'true' ? '|| true' : '';
+  let args = '--no-coverage';
+
   // Include all .test.js files as entry points
   glob.sync(path.resolve(__dirname, '../src/**/*.test.js')).map(f => path.relative(__dirname, f).replace('..', '.')).forEach(f => {
     cfg.entry[path.basename(f, '.js')] = [f];
   });
 
   // Add instrumentation for code coverage checking
-  cfg.module.rules.unshift({
-    test: /\.(js)$/,
-    use: [
-      {
-        loader: 'istanbul-instrumenter-loader',
-        options: {
-          esModules: true,
-          produceSourceMap: true
+  if (process.env.ENABLE_COVERAGE === 'true') {
+    cfg.module.rules.unshift({
+      test: /\.(js)$/,
+      use: [
+        {
+          loader: 'istanbul-instrumenter-loader',
+          options: {
+            esModules: true,
+            produceSourceMap: true
+          }
         }
-      }
-    ],
-    enforce: 'post',
-    exclude: /node_modules|\.tests\.js$/
-  });
+      ],
+      enforce: 'post',
+      exclude: /node_modules|\.tests\.js$/
+    });
+    args = `--100`;
+  }
+
+  // Add plugin to execute test runner
+  cfg.plugins.push(new WebpackShellPlugin({
+    onBuildExit: [{
+      command: `sh`,
+      args: [
+        `-c`,
+        `./node_modules/.bin/tap ${args} --nyc-arg='--instrument=false' --reporter classic -J ./dist/*.test.js && echo '\n✓ ALL TESTS PASSED!' ${onFail}`
+      ]
+    }]
+  }));
 
   return cfg;
 };
