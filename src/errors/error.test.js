7c0e0d77ceb6b228930837f03c2bd7272b6222c2
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2018-02-22 12:54:02 +0000

    Improve errors modules
    The internalServerError middleware was swallowing the errors without
    logging them. We log them using the logger in registered in the request
    (pino middleware).
    
    We also add a test for the 404 error middleware.

--- src/errors/error.test.js
+++ src/errors/error.test.js
@@ -1,10 +1,16 @@
 import {test} from 'tap';
 import express from 'express';
 import request from 'supertest';
-import {internalServerErrorMiddleware} from '.';
+import pino from 'pino';
+import pinoMiddleware from 'express-pino-logger';
+import {internalServerErrorMiddleware, pageNotFoundMiddleware} from '.';
+
+const logBuffer = Buffer.from([]);
+const logger = pino({}, logBuffer);
+const app = express();
+app.use(pinoMiddleware(logger));
 
 test('should display an internal-server-error 500 error page for errors', async t => {
-  const app = express();
   app.use('/bang', (_req, _res, _next) => {
     throw new Error('bang');
   });
@@ -15,3 +21,10 @@ test('should display an internal-server-error 500 error page for errors', async 
     .expect(500);
 });
 
+test('should display an not-found 404 error page for missing pages', async t => {
+  app.use(pageNotFoundMiddleware);
+  return request(app)
+    .get('/not-to-exist')
+    .expect(/Page not found/i)
+    .expect(404);
+});
