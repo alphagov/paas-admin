f999e1ee029b0353f381b16b569401f9a0706cff
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2018-03-09 17:11:11 +0000

    Satisfy tests for spaces module
    We'd like to cover the spaces module as much as possible. We think these
    should go in a separate commit in order to keep a bing chunk of code
    separated.

--- src/cf/client.test.data.js
+++ src/cf/client.test.data.js
@@ -49,12 +49,86 @@ export const organizations = `{
   ]
 }`;
 
+export const organization = `{
+  "metadata": {
+    "guid": "a7aff246-5f5b-4cf8-87d8-f316053e4a20",
+    "url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20",
+    "created_at": "2016-06-08T16:41:33Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "the-system_domain-org-name",
+    "billing_enabled": false,
+    "quota_definition_guid": "dcb680a9-b190-4838-a3d2-b84aa17517a6",
+    "status": "active",
+    "quota_definition_url": "/v2/quota_definitions/dcb680a9-b190-4838-a3d2-b84aa17517a6",
+    "spaces_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/spaces",
+    "domains_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/domains",
+    "private_domains_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/private_domains",
+    "users_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/users",
+    "managers_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/managers",
+    "billing_managers_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/billing_managers",
+    "auditors_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/auditors",
+    "app_events_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/app_events",
+    "space_quota_definitions_url": "/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/space_quota_definitions"
+  }
+}`;
+
+export const organizationQuota = `{
+  "metadata": {
+    "guid": "80f3e539-a8c0-4c43-9c72-649df53da8cb",
+    "url": "/v2/quota_definitions/80f3e539-a8c0-4c43-9c72-649df53da8cb",
+    "created_at": "2016-06-08T16:41:39Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "name-1996",
+    "non_basic_services_allowed": true,
+    "total_services": 60,
+    "total_routes": 1000,
+    "total_private_domains": -1,
+    "memory_limit": 20480,
+    "trial_db_allowed": false,
+    "instance_memory_limit": -1,
+    "app_instance_limit": -1,
+    "app_task_limit": -1,
+    "total_service_keys": -1,
+    "total_reserved_route_ports": 5
+  }
+}`;
+
 export const spaces = `{
   "total_results": 1,
   "total_pages": 1,
   "prev_url": null,
   "next_url": null,
   "resources": [
+    {
+      "metadata": {
+        "guid": "5489e195-c42b-4e61-bf30-323c331ecc01",
+        "url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01",
+        "created_at": "2016-06-08T16:41:35Z",
+        "updated_at": "2016-06-08T16:41:26Z"
+      },
+      "entity": {
+        "name": "name-1774",
+        "organization_guid": "3deb9f04-b449-4f94-b3dd-c73cefe5b275",
+        "space_quota_definition_guid": "a9097bc8-c6cf-4a8f-bc47-623fa22e8019",
+        "allow_ssh": true,
+        "organization_url": "/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275",
+        "developers_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/developers",
+        "managers_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/managers",
+        "auditors_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/auditors",
+        "apps_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/apps",
+        "routes_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/routes",
+        "domains_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/domains",
+        "service_instances_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/service_instances",
+        "app_events_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/app_events",
+        "events_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/events",
+        "security_groups_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/security_groups",
+        "staging_security_groups_url": "/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/staging_security_groups"
+      }
+    },
     {
       "metadata": {
         "guid": "5489e195-c42b-4e61-bf30-323c331ecc01",
@@ -82,6 +156,122 @@ export const spaces = `{
       }
     }
   ]
+}`;
+
+export const space = `{
+  "guid": "50ae42f6-346d-4eca-9e97-f8c9e04d5fbe",
+  "name": "name-1382",
+  "apps": [
+    {
+      "guid": "c49aac23-e26c-4564-ba1a-0b0bdcff1387",
+      "urls": [
+        "host-7.domain-7.example.com"
+      ],
+      "routes": [
+        {
+          "guid": "9b593acf-f617-4713-b9cf-c959aa2e3276",
+          "host": "host-7",
+          "port": null,
+          "path": "",
+          "domain": {
+            "guid": "97b03bcb-db5d-4923-8b02-662c490fc2ac",
+            "name": "domain-7.example.com"
+          }
+        }
+      ],
+      "service_count": 1,
+      "service_names": [
+        "name-1385"
+      ],
+      "running_instances": 0,
+      "name": "name-1388",
+      "production": false,
+      "space_guid": "50ae42f6-346d-4eca-9e97-f8c9e04d5fbe",
+      "stack_guid": "8d4152c3-d2bb-44ac-9036-b2a8c95f24ef",
+      "buildpack": null,
+      "detected_buildpack": null,
+      "detected_buildpack_guid": null,
+      "environment_json": null,
+      "memory": 1024,
+      "instances": 1,
+      "disk_quota": 1024,
+      "state": "STOPPED",
+      "version": "a93316d0-dcd0-43c2-9569-624af35a61bd",
+      "command": null,
+      "console": false,
+      "debug": null,
+      "staging_task_id": null,
+      "package_state": "PENDING",
+      "health_check_type": "port",
+      "health_check_timeout": null,
+      "staging_failed_reason": null,
+      "staging_failed_description": null,
+      "diego": false,
+      "docker_image": null,
+      "docker_credentials": {
+        "username": null,
+        "password": null
+      },
+      "package_updated_at": "2016-06-08T16:41:28Z",
+      "detected_start_command": "",
+      "enable_ssh": true,
+      "ports": null
+    }
+  ],
+  "services": [
+    {
+      "guid": "5cf08d8b-848c-4f27-bd92-8080fa021783",
+      "name": "name-1385",
+      "bound_app_count": 1,
+      "shared_from": {
+        "space_guid": "c4861ea6-fc27-4a20-ad21-461743ce8921",
+        "space_name": "source-space",
+        "organization_name": "source-org"
+      },
+      "last_operation": {
+        "type": "create",
+        "state": "succeeded",
+        "description": "description goes here",
+        "updated_at": "2016-06-08T16:41:28Z",
+        "created_at": "2016-06-08T16:41:28Z"
+      },
+      "dashboard_url": null,
+      "service_plan": {
+        "guid": "0f8ad3ee-ca65-4849-ae52-d6539392fae2",
+        "name": "name-1386",
+        "service": {
+          "guid": "17752c54-ac25-46fb-9989-3f69017d0dbe",
+          "label": "label-27",
+          "provider": null,
+          "version": null
+        }
+      }
+    }
+  ]
+}`;
+
+export const spaceQuota = `{
+  "metadata": {
+    "guid": "a9097bc8-c6cf-4a8f-bc47-623fa22e8019",
+    "url": "/v2/space_quota_definitions/a9097bc8-c6cf-4a8f-bc47-623fa22e8019",
+    "created_at": "2016-06-08T16:41:29Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "name-1491",
+    "organization_guid": "a065dfc7-3aed-4438-a6af-b1f42d9a4ed4",
+    "non_basic_services_allowed": true,
+    "total_services": 60,
+    "total_routes": 1000,
+    "memory_limit": 20480,
+    "instance_memory_limit": -1,
+    "app_instance_limit": -1,
+    "app_task_limit": 5,
+    "total_service_keys": 600,
+    "total_reserved_route_ports": -1,
+    "organization_url": "/v2/organizations/a065dfc7-3aed-4438-a6af-b1f42d9a4ed4",
+    "spaces_url": "/v2/space_quota_definitions/a9097bc8-c6cf-4a8f-bc47-623fa22e8019/spaces"
+  }
 }`;
 
 export const apps = `{
--- src/spaces/spaces.test.js
+++ src/spaces/spaces.test.js
@@ -3,10 +3,16 @@ import express from 'express';
 import nock from 'nock';
 import request from 'supertest';
 import {Client} from '../cf';
-import {spaces} from '../cf/client.test.data';
+import * as data from '../cf/client.test.data';
 import spacesApp from '.';
 
-nock('https://example.com').get('/api/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275/spaces').times(1).reply(200, spaces);
+nock('https://example.com/api')
+  .get('/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275').times(1).reply(200, data.organization)
+  .get('/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275/spaces').times(1).reply(200, data.spaces)
+  .get('/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275/user_roles').times(1).reply(200, data.users)
+  .get('/v2/quota_definitions/dcb680a9-b190-4838-a3d2-b84aa17517a6').times(1).reply(200, data.organizationQuota)
+  .get('/v2/spaces/5489e195-c42b-4e61-bf30-323c331ecc01/summary').times(2).reply(200, data.space)
+  .get('/v2/space_quota_definitions/a9097bc8-c6cf-4a8f-bc47-623fa22e8019').times(1).reply(200, data.spaceQuota);
 
 const app = express();
 
