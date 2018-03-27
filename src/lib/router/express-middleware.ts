e8ee7d323228fcb42166a9890685dc06565bdef7
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2018-03-27 19:07:48 +0100

    Add router

---
+++ src/lib/router/express-middleware.ts
@@ -0,0 +1,47 @@
+import express from 'express';
+
+import { initContext } from '../../app/context';
+
+import { IResponse } from './route';
+import Router from './router';
+
+function handleResponse(res: express.Response) {
+  return (r: IResponse) => {
+    if (r.redirect) {
+      return res.redirect(r.redirect);
+    }
+
+    res.status(r.status || 200).send(r.body);
+  };
+}
+
+export function expressMiddleware(router: Router): express.Application {
+  const app = express();
+
+  app.use((req: any, _res: express.Response, next: express.NextFunction) => {
+    req.router = router;
+    next();
+  });
+
+  app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
+    let route;
+    try {
+      route = router.find(req.path, req.method);
+    } catch (err) {
+      return next(err);
+    }
+
+    const params = {
+      ...req.params,
+      ...route.parser.match(req.path),
+    };
+
+    const ctx = initContext(req, router);
+
+    route.definition.action(ctx, params, req.body)
+      .then(handleResponse(res))
+      .catch(next);
+  });
+
+  return app;
+}
