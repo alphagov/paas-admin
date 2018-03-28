90a5e875c77326ba30bf6858b9d965898ec76461
Rafal Proszowski <rafal.proszowski@digital.cabinet-office.gov.uk>
2018-03-28 18:31:34 +0100

    Adapt spaces module to TypeScript

--- src/spaces/spaces.test.ts
+++ src/spaces/spaces.test.ts
@@ -1,10 +1,14 @@
-import {test} from 'tap';
-import express from 'express';
 import nock from 'nock';
-import request from 'supertest';
-import {CloudFoundryClient} from '../cf';
+import pino from 'pino';
+import { test } from 'tap';
+
+import { IContext } from '../app/context';
+import CloudFoundryClient from '../cf';
 import * as data from '../cf/cf.test.data';
-import spacesApp from '.';
+import NotificationClient from '../notify';
+import UAAClient from '../uaa';
+
+import * as spaces from '.';
 
 nock('https://example.com/api')
   .get('/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275').times(1).reply(200, data.organization)
@@ -20,33 +24,37 @@ nock('https://example.com/api')
   .get('/v2/spaces/bc8d3381-390d-4bd7-8c71-25309900a2e3').times(1).reply(200, data.space)
   .get('/v2/space_quota_definitions/a9097bc8-c6cf-4a8f-bc47-623fa22e8019').times(1).reply(200, data.spaceQuota);
 
-const app = express();
-
-app.use((req, res, next) => {
-  req.log = console;
-  next();
-});
-
-app.use((req, res, next) => {
-  req.cf = new CloudFoundryClient({
+const ctx: IContext = {
+  cf: new CloudFoundryClient({
     apiEndpoint: 'https://example.com/api',
-    accessToken: 'qwerty123456'
-  });
-  next();
-});
-
-app.use(spacesApp);
+    accessToken: '__CF_ACCESS_TOKEN__',
+  }),
+  linkTo: () => '__LINKED_TO__',
+  log: pino({level: 'silent'}),
+  notify: new NotificationClient({apiKey: '__NOTIFY_KEY__', templates: {}}),
+  rawToken: {user_id: '__USER_ID__', scope: []},
+  uaa: new UAAClient({
+    apiEndpoint: 'https://example.com/uaa',
+    clientCredentials: {
+      clientID: '__UAA_CLIENT_ID__',
+      clientSecret: '__UAA_CLIENT_SECRET__',
+    },
+  }),
+};
 
 test('should show the spaces pages', async t => {
-  const response = await request(app).get('/3deb9f04-b449-4f94-b3dd-c73cefe5b275');
+  const response = await spaces.listSpaces(ctx, {
+    organizationGUID: '3deb9f04-b449-4f94-b3dd-c73cefe5b275',
+  });
 
-  t.equal(response.status, 200);
-  t.contains(response.text, 'Spaces');
+  t.contains(response.body, 'Spaces');
 });
 
 test('should space overview', async t => {
-  const response = await request(app).get('/bc8d3381-390d-4bd7-8c71-25309900a2e3/overview');
+  const response = await spaces.listApplications(ctx, {
+    organizationGUID: '3deb9f04-b449-4f94-b3dd-c73cefe5b275',
+    spaceGUID: 'bc8d3381-390d-4bd7-8c71-25309900a2e3',
+  });
 
-  t.equal(response.status, 200);
-  t.contains(response.text, 'name-1382 - Overview');
+  t.contains(response.body, 'name-1382 - Overview');
 });
---
+++ src/spaces/spaces.ts
@@ -0,0 +1,98 @@
+import { IContext } from '../app/context';
+import * as cf from '../cf/types';
+import { IParameters, IResponse } from '../lib/router';
+
+import spaceOverviewTemplate from './overview.njk';
+import spacesTemplate from './spaces.njk';
+
+function buildURL(route: cf.IRoute): string {
+  return [route.host, route.domain.name].filter(x => x).join('.') + route.path;
+}
+
+export async function listApplications(ctx: IContext, params: IParameters): Promise<IResponse> {
+  const space = await ctx.cf.space(params.spaceGUID);
+  const applications = await ctx.cf.applications(params.spaceGUID);
+  const organization = await ctx.cf.organization(space.entity.organization_guid);
+
+  const summarisedSpace = {
+    entity: {
+      ...space.entity,
+      ...await ctx.cf.spaceSummary(space.metadata.guid),
+    },
+    metadata: space.metadata,
+  };
+
+  const summarisedApplications = await Promise.all(applications.map(async (application: cf.IApplication) => {
+    const summary = await ctx.cf.applicationSummary(application.metadata.guid);
+
+    return {
+      metadata: application.metadata,
+      entity: {
+        ...application.entity,
+        ...summary,
+
+        urls: summary.routes.map(buildURL),
+      },
+    };
+  }));
+
+  return {
+    body: spaceOverviewTemplate.render({
+      applications: summarisedApplications,
+      linkTo: ctx.linkTo,
+      organization,
+      space: summarisedSpace,
+    }),
+  };
+}
+
+export async function listSpaces(ctx: IContext, params: IParameters): Promise<IResponse> {
+  const spaces = await ctx.cf.spaces(params.organizationGUID);
+  const organization = await ctx.cf.organization(params.organizationGUID);
+  const users = await ctx.cf.usersForOrganization(params.organizationGUID);
+  const managers = users.filter((user: cf.IOrganizationUserRoles) =>
+    user.entity.organization_roles.some(role => role === 'org_manager'),
+  );
+
+  const summarisedSpaces = await Promise.all(spaces.map(async (space: cf.ISpace) => {
+    const summary = await ctx.cf.spaceSummary(space.metadata.guid);
+
+    return {
+      entity: {
+        ...space.entity,
+        ...summary,
+
+        running_apps: summary.apps.filter((app: cf.IApplicationSummary) => app.running_instances > 0),
+        stopped_apps: summary.apps.filter((app: cf.IApplicationSummary) => app.running_instances === 0),
+        memory_allocated: summary.apps.reduce((allocated: number, app: cf.IApplicationSummary) =>
+          allocated + app.memory, 0),
+        quota: space.entity.space_quota_definition_guid ?
+          await ctx.cf.spaceQuota(space.entity.space_quota_definition_guid) : null,
+      },
+      metadata: space.metadata,
+    };
+  }));
+
+  const summerisedOrganization = {
+    entity: {
+      ...organization.entity,
+
+      quota: await ctx.cf.organizationQuota(organization.entity.quota_definition_guid),
+      memory_allocated: summarisedSpaces
+        .reduce((allocated: number, space: {entity: {memory_allocated: number}}) => {
+          return allocated + space.entity.memory_allocated;
+        }, 0),
+    },
+    metadata: organization.metadata,
+  };
+
+  return {
+    body: spacesTemplate.render({
+      linkTo: ctx.linkTo,
+      managers,
+      organization: summerisedOrganization,
+      spaces: summarisedSpaces,
+      users,
+    }),
+  };
+}
