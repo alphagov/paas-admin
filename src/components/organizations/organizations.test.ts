68046c8d52a68091a9945cf7b70be766bd3723b3
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2018-09-26 13:21:24 +0100

    Sort organizations by name
    Note: copying the array because JS's `Array.prototype.sort` function
    mutates the input and the API returns a "ReadonlyArray<T>". We could
    cast the result to an array and sort it in place, but given there are
    going to be ~< 100 entries it should be fairly cheap to copy it.

--- src/components/organizations/organizations.test.ts
+++ src/components/organizations/organizations.test.ts
@@ -2,15 +2,49 @@ import jwt from 'jsonwebtoken';
 import nock from 'nock';
 import pino from 'pino';
 
-import { organizations } from '../../lib/cf/cf.test.data';
-
 import { config } from '../app/app.test.config';
 import { IContext } from '../app/context';
 import { Token } from '../auth';
 
 import { listOrganizations } from '.';
 
-nock('https://example.com/api').get('/v2/organizations').times(1).reply(200, organizations);
+const organizationTemplate = (name: string, guid: string) => `{
+  "metadata": {
+    "guid": "${guid}",
+    "url": "/v2/organizations/${guid}",
+    "created_at": "2016-06-08T16:41:33Z",
+    "updated_at": "2016-06-08T16:41:26Z"
+  },
+  "entity": {
+    "name": "${name}",
+    "billing_enabled": false,
+    "quota_definition_guid": "dcb680a9-b190-4838-a3d2-b84aa17517a6",
+    "status": "active",
+    "quota_definition_url": "/v2/quota_definitions/dcb680a9-b190-4838-a3d2-b84aa17517a6",
+    "spaces_url": "/v2/organizations/${guid}/spaces",
+    "domains_url": "/v2/organizations/${guid}/domains",
+    "private_domains_url": "/v2/organizations/${guid}/private_domains",
+    "users_url": "/v2/organizations/${guid}/users",
+    "managers_url": "/v2/organizations/${guid}/managers",
+    "billing_managers_url": "/v2/organizations/${guid}/billing_managers",
+    "auditors_url": "/v2/organizations/${guid}/auditors",
+    "app_events_url": "/v2/organizations/${guid}/app_events_url",
+    "space_quota_definitions_url": "/v2/organizations/${guid}/space_quota_definitions"
+  }
+}`;
+const organizations = `{
+  "total_results": 1,
+  "total_pages": 1,
+  "prev_url": null,
+  "next_url": null,
+  "resources": [
+    ${organizationTemplate('c-org-name-1', 'a7aff246-5f5b-4cf8-87d8-f316053e4a20')},
+    ${organizationTemplate('d-org-name-2', 'a7aff246-5f5b-4cf8-87d8-f316053e4a21')},
+    ${organizationTemplate('b-org-name-3', 'a7aff246-5f5b-4cf8-87d8-f316053e4a22')},
+    ${organizationTemplate('a-org-name-4', 'a7aff246-5f5b-4cf8-87d8-f316053e4a23')}
+  ]
+}`;
+nock('https://example.com/api').get('/v2/organizations').times(2).reply(200, organizations);
 
 const tokenKey = 'secret';
 const token = jwt.sign({
@@ -32,4 +66,28 @@ describe('organizations test suite', () => {
 
     expect(response.body).toContain('Choose an organisation');
   });
+
+  it('should sort the organisations by name', async () => {
+    const response = await listOrganizations(ctx, {});
+
+    const matches = extractOrganizations(response.body as string);
+    expect(matches.length).toBe(4);
+    expect(matches[0]).toBe('a-org-name-4');
+    expect(matches[1]).toBe('b-org-name-3');
+    expect(matches[2]).toBe('c-org-name-1');
+    expect(matches[3]).toBe('d-org-name-2');
+  });
 });
+
+function extractOrganizations(responseBody: string): ReadonlyArray<string> {
+    const re = /(.-org-name-\d)/g;
+    const matches = [];
+    while (true) {
+      const match = re.exec(responseBody);
+      if (match) {
+        matches.push(match[0]);
+      } else {
+        return matches;
+      }
+    }
+}
--- src/components/organizations/organizations.ts
+++ src/components/organizations/organizations.ts
@@ -1,9 +1,16 @@
 import CloudFoundryClient from '../../lib/cf';
+import { IOrganization } from '../../lib/cf/types';
 import { IParameters, IResponse } from '../../lib/router';
 
 import { IContext } from '../app/context';
 
 import organizationsTemplate from './organizations.njk';
+
+function sortOrganizationsByName(organizations: ReadonlyArray<IOrganization>): ReadonlyArray<IOrganization> {
+  const organizationsCopy = Array.from(organizations);
+  organizationsCopy.sort((a, b) => a.entity.name.localeCompare(b.entity.name));
+  return organizationsCopy;
+}
 
 export async function listOrganizations(ctx: IContext, _params: IParameters): Promise<IResponse> {
   const cf = new CloudFoundryClient({
@@ -11,7 +18,7 @@ export async function listOrganizations(ctx: IContext, _params: IParameters): Pr
     apiEndpoint: ctx.app.cloudFoundryAPI,
   });
 
-  const organizations = await cf.organizations();
+  const organizations = await cf.organizations().then(sortOrganizationsByName);
   const cfDownloadLinkLocation = 'https://packages.cloudfoundry.org/stable?release=';
   const cfDownloadLinkSource = '&amp;source=github';
   const documentationLink = 'https://docs.cloud.service.gov.uk/#';
