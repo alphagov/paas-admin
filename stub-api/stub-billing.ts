a9a99690ccf3226b40747c1d75dd0fa9a9e0dea0
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-04-18 17:58:04 +0100

    [#165418008] Stub APIs needed for the calculator
    This isn't good enough to actually use the calculator, but it at least
    renders most of the components.

--- stub-api/stub-billing.ts
+++ stub-api/stub-billing.ts
@@ -13,23 +13,23 @@ const defaultPriceDetails = {
   currency_rate: "0.8"
 };
 const defaultBillingEvent = {
-      event_guid: "default-event-guid",
-      event_start: "2019-01-22T10:44:05+00:00",
-      event_stop: "2019-01-23T14:42:55+00:00",
-      resource_guid: "default-resource-guid",
-      resource_name: "default-resource",
-      resource_type: "app",
-      org_guid: "some-org-guid",
-      org_name: "some-org",
-      space_guid: "some-space-guid",
-      space_name: "some-space",
-      plan_guid: "some-plan-guid",
-      quota_definition_guid: null,
-      number_of_nodes: 2,
-      memory_in_mb: 128,
-      storage_in_mb: 0,
-      price: {}
-    }
+  event_guid: "default-event-guid",
+  event_start: "2019-01-22T10:44:05+00:00",
+  event_stop: "2019-01-23T14:42:55+00:00",
+  resource_guid: "default-resource-guid",
+  resource_name: "default-resource",
+  resource_type: "app",
+  org_guid: "some-org-guid",
+  org_name: "some-org",
+  space_guid: "some-space-guid",
+  space_name: "some-space",
+  plan_guid: "some-plan-guid",
+  quota_definition_guid: null,
+  number_of_nodes: 2,
+  memory_in_mb: 128,
+  storage_in_mb: 0,
+  price: {}
+};
 function mockBilling(app: express.Application, _config: { stubApiPort: string, adminPort: string }) {
   app.get('/billable_events', (_req, res) => {
     res.send(JSON.stringify([
@@ -64,6 +64,110 @@ function mockBilling(app: express.Application, _config: { stubApiPort: string, a
         price: {inc_vat: "100.00", ex_vat: "80.00", details: [{...defaultPriceDetails, plan_name: "magical-plan"}]}
       },
     ]));
+  });
+  app.get('/pricing_plans', (_req, res) => {
+    res.send(JSON.stringify([
+        {
+          name: "app",
+          plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798c4",
+          valid_from: "2017-01-01T00:00:00+00:00",
+          components: [
+            {
+              name: "instance",
+              formula: "ceil($time_in_seconds/3600) * 0.01",
+              vat_code: "Standard",
+              currency_code: "USD"
+            }
+          ],
+          memory_in_mb: 0,
+          storage_in_mb: 524288,
+          number_of_nodes: 0
+        },
+        {
+          name: "postgres tiny-9.6",
+          plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798c5",
+          valid_from: "2017-01-01T00:00:00+00:00",
+          components: [],
+          memory_in_mb: 0,
+          storage_in_mb: 0,
+          number_of_nodes: 0
+        },
+        {
+          name: "postgres massive-9.6",
+          plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798a5",
+          valid_from: "2017-01-01T00:00:00+00:00",
+          components: [],
+          memory_in_mb: 0,
+          storage_in_mb: 0,
+          number_of_nodes: 0
+        },
+        {
+          name: "mysql large-ha-5.7",
+          plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798c6",
+          valid_from: "2017-01-01T00:00:00+00:00",
+          components: [],
+          memory_in_mb: 0,
+          storage_in_mb: 0,
+          number_of_nodes: 0
+        },
+        {
+          name: "redis tiny-clustered-3.2",
+          plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798c7",
+          valid_from: "2017-01-01T00:00:00+00:00",
+          components: [],
+          memory_in_mb: 0,
+          storage_in_mb: 0,
+          number_of_nodes: 0
+        },
+        {
+          name: "elasticsearch small-ha-6.x",
+          plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798c8",
+          valid_from: "2017-01-01T00:00:00+00:00",
+          components: [],
+          memory_in_mb: 0,
+          storage_in_mb: 0,
+          number_of_nodes: 0
+        },
+        {
+          name: "prometheus",
+          valid_from: "2002-01-01",
+          components: []
+        }
+    ]))
+  });
+  app.get('/forecast_events', (_req, res) => {
+    res.send(JSON.stringify([{
+      event_guid: "aa30fa3c-725d-4272-9052-c7186d4968a6",
+      event_start: "2001-01-01T00:00:00+00:00",
+      event_stop: "2001-01-01T01:00:00+00:00",
+      resource_guid: "c85e98f0-6d1b-4f45-9368-ea58263165a0",
+      resource_name: "APP1",
+      resource_type: "_TESTING_APPLICATION_",
+      org_guid: "51ba75ef-edc0-47ad-a633-a8f6e8770944",
+      space_guid: "276f4886-ac40-492d-a8cd-b2646637ba76",
+      plan_guid: "f4d4b95a-f55e-4593-8d54-3364c25798c4",
+      number_of_nodes: 1,
+      memory_in_mb: 1024,
+      storage_in_mb: 0,
+      price: {
+        inc_vat: "0.012",
+        ex_vat: "0.01",
+        details: [
+          {
+            name: "compute",
+            plan_name: "PLAN1",
+            start: "2001-01-01T00:00:00+00:00",
+            stop: "2001-01-01T01:00:00+00:00",
+            vat_rate: "0.2",
+            vat_code: "Standard",
+            currency_code: "GBP",
+            currency_rate: "1",
+              inc_vat: "0.012",
+              ex_vat: "0.01"
+            }
+          ]
+        }
+      }]))
   });
 };
 
