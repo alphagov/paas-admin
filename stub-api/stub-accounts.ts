4c089cd615c291ca329ba6ea2a7c7d6b9ef79523
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-07-20 20:42:30 +0100

    Refactor stub-accounts to have a response builder
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- stub-api/stub-accounts.ts
+++ stub-api/stub-accounts.ts
@@ -1,8 +1,16 @@
 import express from 'express';
-import {IAccountsUserResponse} from '../src/lib/accounts';
+import {IAccountsUserResponse, IAccountsUsersResponse} from '../src/lib/accounts';
 import * as cfStubData from '../src/lib/cf/cf.test.data';
 import * as uaaStubData from '../src/lib/uaa/uaa.test.data';
 import {IStubServerPorts} from './index';
+
+function makeUser(id: string): IAccountsUserResponse {
+  return {
+    user_uuid: id,
+    username: `${id}@fake.cabinet-office.gov.uk`,
+    user_email: `${id}@fake.cabinet-office.gov.uk`,
+  };
+}
 
 function mockAccounts(app: express.Application, _config: IStubServerPorts): express.Application {
   // All users have no documents
@@ -23,13 +31,7 @@ function mockAccounts(app: express.Application, _config: IStubServerPorts): expr
 
     const id: string = req.params.guid;
     if (userIds.indexOf(id) >= 0) {
-      const userBody: IAccountsUserResponse = {
-        user_uuid: id,
-        username: `${id}@fake.cabinet-office.gov.uk`,
-        user_email: `${id}@fake.cabinet-office.gov.uk`,
-      };
-
-      res.send(JSON.stringify(userBody));
+      res.send(JSON.stringify(makeUser(id)));
     } else {
       res.status(404).send('Not found');
     }
