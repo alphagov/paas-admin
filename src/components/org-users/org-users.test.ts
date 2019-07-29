b0b1e1ee7286ab7bfdb4bb8f25c8ce2cd1b2e947
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-07-29 11:53:33 +0100

    org-users remove unnused nock
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- src/components/org-users/org-users.test.ts
+++ src/components/org-users/org-users.test.ts
@@ -75,8 +75,6 @@ describe('org-users test suite', () => {
 
     // tslint:disable:max-line-length
     nockCF
-      .get('/v2/users/uaa-id-253/spaces?q=organization_guid:3deb9f04-b449-4f94-b3dd-c73cefe5b275')
-      .reply(200, cfData.spaces)
       .get('/v2/organizations/a7aff246-5f5b-4cf8-87d8-f316053e4a20/spaces')
       .reply(200, cfData.spaces)
       .get('/v2/organizations/3deb9f04-b449-4f94-b3dd-c73cefe5b275/spaces')
