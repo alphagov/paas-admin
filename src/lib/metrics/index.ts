1cb3f416d263d068bf2d10de74e739a6c4982ddc
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-08-22 08:00:26 +0100

    Remove "ago" from nice time offsets
    "5 minutes ago" or "Last 5 minutes"
    
    Latter sounds better
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- src/lib/metrics/index.ts
+++ src/lib/metrics/index.ts
@@ -1,10 +1,10 @@
 export const timeOffsets: {readonly [key: string]: number} = {
-  '5-minutes-ago': 5 * 60,
-  '15-minutes-ago': 15 * 60,
-  '30-minutes-ago': 30 * 60,
-  '1-hour-ago': 60 * 60,
-  '3-hours-ago': 3 * 60 * 60,
-  '6-hours-ago': 6 * 60 * 60,
-  '12-hours-ago': 12 * 60 * 60,
-  '24-hours-ago': 24 * 60 * 60,
+  'last-5-minutes': 5 * 60,
+  'last-15-minutes': 15 * 60,
+  'last-30-minutes': 30 * 60,
+  'last-1-hour': 60 * 60,
+  'last-3-hours': 3 * 60 * 60,
+  'last-6-hours': 6 * 60 * 60,
+  'last-12-hours': 12 * 60 * 60,
+  'last-24-hours': 24 * 60 * 60,
 };
