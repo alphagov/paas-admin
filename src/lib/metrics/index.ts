4fb31fa66048d7162b34b35834c970f2eda57a1e
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-08-22 08:01:49 +0100

    Add additional times to metrics nice time offsets
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- src/lib/metrics/index.ts
+++ src/lib/metrics/index.ts
@@ -7,4 +7,6 @@ export const timeOffsets: {readonly [key: string]: number} = {
   'last-6-hours': 6 * 60 * 60,
   'last-12-hours': 12 * 60 * 60,
   'last-24-hours': 24 * 60 * 60,
+  'last-2-days': 2 * 24 * 60 * 60,
+  'last-7-days': 7 * 24 * 60 * 60,
 };
