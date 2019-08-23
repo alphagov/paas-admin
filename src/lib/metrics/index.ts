2df5ab799d7c1f32ec962a76b446fd93f710956e
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-08-23 15:50:17 +0100

    Reduce the number of metrics time ranges available
    for usability reasons fewer is better
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

--- src/lib/metrics/index.ts
+++ src/lib/metrics/index.ts
@@ -1,10 +1,7 @@
 export const timeOffsets: {readonly [key: string]: number} = {
-  'last-5-minutes': 5 * 60,
   'last-15-minutes': 15 * 60,
-  'last-30-minutes': 30 * 60,
   'last-1-hour': 60 * 60,
-  'last-3-hours': 3 * 60 * 60,
-  'last-6-hours': 6 * 60 * 60,
+  'last-4-hours': 4 * 60 * 60,
   'last-12-hours': 12 * 60 * 60,
   'last-24-hours': 24 * 60 * 60,
   'last-2-days': 2 * 24 * 60 * 60,
