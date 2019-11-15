1b3f457f3653d7a17744c85c732ab7b4c6e78afb
Richard Towers <richard.towers@digital.cabinet-office.gov.uk>
2019-11-15 18:22:08 +0000

    Add awsCloudwatchEndpoint to IAppConfig
    This will be required so we can point the cloudwatch client at a
    non-production URL for tests.

--- src/components/service-metrics/service-metrics.ts
+++ src/components/service-metrics/service-metrics.ts
@@ -1,12 +1,18 @@
 import * as cw from '@aws-sdk/client-cloudwatch-node';
 
 import { getMetricWidget } from '../../lib/aws/cloudwatch-metric-widgets';
+import moment from 'moment';
+import { CloudwatchMetricDataClient } from '../../lib/aws/cloudwatch-metric-data';
 import CloudFoundryClient from '../../lib/cf';
+import roundDown from '../../lib/moment/round';
 import { IParameters, IResponse } from '../../lib/router';
 import { IContext } from '../app';
 import { CLOUD_CONTROLLER_ADMIN, CLOUD_CONTROLLER_GLOBAL_AUDITOR, CLOUD_CONTROLLER_READ_ONLY_ADMIN } from '../auth';
 import { fromOrg, IBreadcrumb } from '../breadcrumbs';
-import serviceMetricsTemplate from './service-metrics.njk';
+import { drawMultipleLineGraphs } from '../charts/line-graph';
+import elasticacheServiceMetricsTemplate from './elasticache-service-metrics.njk';
+import rdsServiceMetricsTemplate from './rds-service-metrics.njk';
+import unsupportedServiceMetricsTemplate from './unsupported-service-metrics.njk';
 
 // Ignoring test coverage of viewServiceMetricImage because this is temporary code,
 // to be replaced with properly rendered SVG graphs at a later date.
@@ -85,19 +91,62 @@ export async function viewServiceMetrics(ctx: IContext, params: IParameters): Pr
         { text: service.entity.name },
     ]);
 
-    return {
-        body: serviceMetricsTemplate.render({
-            routePartOf: ctx.routePartOf,
-            linkTo: ctx.linkTo,
-            context: ctx.viewContext,
-            organization,
-            service,
-            serviceLabel,
-            space,
-            isAdmin,
-            isBillingManager,
-            isManager,
-            breadcrumbs,
-        }),
+    const cloudWatchMetricDataClient = new CloudwatchMetricDataClient(
+      new cw.CloudWatchClient({
+        region: ctx.app.awsRegion,
+        endpoint: ctx.app.awsCloudwatchEndpoint,
+      }),
+    );
+
+    const period = moment.duration(5, 'minutes');
+    const endTime = roundDown(moment(), period);
+    const startTime = endTime.clone().subtract(1, 'day');
+
+    const metricGraphData = await cloudWatchMetricDataClient.getMetricGraphData(
+      service.metadata.guid,
+      serviceLabel,
+      period,
+      startTime,
+      endTime,
+    );
+
+    const defaultTemplateParams = {
+        routePartOf: ctx.routePartOf,
+        linkTo: ctx.linkTo,
+        context: ctx.viewContext,
+        organization,
+        service,
+        serviceLabel,
+        space,
+        isAdmin,
+        isBillingManager,
+        isManager,
+        breadcrumbs,
     };
+    if (!metricGraphData) {
+      return {
+        body: unsupportedServiceMetricsTemplate.render(defaultTemplateParams),
+      };
+    }
+    const metricGraphsById = drawMultipleLineGraphs(metricGraphData.graphs);
+
+    switch (metricGraphData.serviceType) {
+      case 'rds':
+        return {
+          body: rdsServiceMetricsTemplate.render({
+            ...defaultTemplateParams,
+            metricGraphsById,
+          }),
+        };
+      case 'elasticache':
+        return {
+          body: elasticacheServiceMetricsTemplate.render({
+            ...defaultTemplateParams,
+            metricGraphsById,
+          }),
+        };
+      /* istanbul ignore next */
+      default:
+        throw new Error(`Unrecognised service type ${metricGraphData.serviceType}`);
+    }
 }
