de5b749d6c18702e42904d4cee2d51f6b963357f
Lee Porte <lee.porte@digital.cabinet-office.gov.uk>
2019-12-17 11:13:49 +0000

    Remove Google Analytics cookies
    We are removing the Google Analytics cookies whilst the cross domain approach is being developed.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -18,7 +18,6 @@ let cfg = {
     main: ['./src/main.ts'],
     "assets/init": ['./src/frontend/javascript/init.js'],
     "assets/sankey": ['./src/frontend/javascript/sankey.js'],
-    "assets/analytics": ['./src/frontend/javascript/analytics.js'],
   },
 
   output: {
