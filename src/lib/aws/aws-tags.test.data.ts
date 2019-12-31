33d44d19dccc8ee3657b1db9075d5e0c6d8ee9ff
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2019-12-31 15:52:33 +0000

    Add test for cloudfront metrics being rendered
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>

---
+++ src/lib/aws/aws-tags.test.data.ts
@@ -0,0 +1,15 @@
+export function getStubResourcesByTag(): string {
+  return JSON.stringify({
+    ResourceTagMappingList: [
+      {
+        ComplianceDetails: {
+          ComplianceStatus: true,
+          KeysWithNoncompliantValues: [],
+          NoncompliantKeys: [],
+        },
+        ResourceARN: 'arn:aws:cloudfront::123456789012:distribution/EDFDVBD632BHDS5',
+        Tags: [],
+      },
+    ],
+  });
+}
