7999b67957fc3c52a4c036cd58501c81079d443d
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2020-01-13 18:44:15 +0000

    Add placeholder null vals to elasticsearch metrics
    The charts draw only the metrics you give it, and do not know about the
    start/end times.
    
    As such, if we do not add placeholder values, then we will only get
    metrics over the time period for data returned which could confuse
    users.
    
    Add placeholder values, like in metric-data-getters/cloudwatch so that
    the charts will be drawn according to the start/end time rather than the
    time period for the metrics returned
    
    > As a user, when I see a metrics chart, I expect to see a metrics chart
    for the start/end time, rather than for the metrics returned, so that I
    can be sure that my inputted start/end times were taken into account
    
    Signed-off-by: Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
    Co-authored-by: Sebastian <sebastian.schmieschek@digital.cabinet-office.gov.uk>

--- src/lib/metric-data-getters/elasticsearch.ts
+++ src/lib/metric-data-getters/elasticsearch.ts
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import moment from 'moment';
 
 import {
+  IMetric,
   IMetricDataGetter,
   IMetricSerie,
   MetricName,
@@ -53,6 +54,28 @@ export class ElasticsearchMetricDataGetter implements IMetricDataGetter {
       }
     });
 
-    return Promise.resolve(metricData);
+    const placeholderData: {[key: number]: IMetric} = {};
+    for (const time = rangeStart.clone(); time.isSameOrBefore(rangeStop); time.add(period)) {
+      placeholderData[+time] = {date: time.toDate(), value: NaN};
+    }
+
+    const metricDataWithPlaceholders = _.mapValues(metricData, series => {
+      return series.map(serie => {
+        const metricDataByTimestamp: {[key: number]: IMetric} = {};
+        serie.metrics.forEach(metric => metricDataByTimestamp[+metric.date] = metric);
+
+        const serieWithPlaceholders = {
+          ...placeholderData,
+          ...metricDataByTimestamp,
+        };
+
+        return {
+          label: serie.label,
+          metrics: _.map(serieWithPlaceholders, metric => ({date: metric.date, value: metric.value})),
+        };
+      });
+    });
+
+    return Promise.resolve(metricDataWithPlaceholders);
   }
 }
