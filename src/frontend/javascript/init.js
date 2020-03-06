aa97b8bbdeca919e27e989277b718a366fe98fe5
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2020-03-06 13:20:47 +0000

    Only initialise javascript for components in use
    At the moment we have a console error for the tabs component
    as we're only making it look like tabs,
    but links link to actual pages not content panels
    and the missing attribute throws an error

--- src/frontend/javascript/init.js
+++ src/frontend/javascript/init.js
@@ -1,3 +1,29 @@
-(function() {
-  window.GOVUKFrontend.initAll();
-})();
+// there is ever only one header per page
+var $headerMenuButton = document.querySelector('[data-module="govuk-header"]');
+var GOVUKHeader = window.GOVUKFrontend.Header;
+if ($headerMenuButton) {
+  new GOVUKHeader($headerMenuButton).init();
+}
+
+var $buttons = document.querySelectorAll('[data-module="govuk-button"]');
+var GOVUKButton = window.GOVUKFrontend.Button;
+if ($buttons) {
+  for (var i = 0; i < $buttons.length; i++) {
+    new GOVUKButton($button).init();
+  };
+}
+
+var $details = document.querySelectorAll('[data-module="govuk-details"]');
+var GOVUKDetails = window.GOVUKFrontend.Details;
+if ($details) {
+  for (var i = 0; i < $details.length; i++) {
+    new GOVUKDetails($detail).init();
+  };
+}
+
+// there is ever only one error summuary per page
+var $errorSummary = document.querySelector('[data-module="govuk-error-summary"]');
+var GOVUKErrorSummary = window.GOVUKFrontend.ErrorSummary;
+if ($errorSummary) {
+  new GOVUKErrorSummary($errorSummary).init();
+}
