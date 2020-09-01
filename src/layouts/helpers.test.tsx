4a7a048c12a2c1a5020fb1c417064d13fbd347fa
kr8n3r <jani@kraner.dev>
2020-09-01 09:02:45 +0100

    Add tooltip helper tests & update other tests

--- src/layouts/helpers.test.tsx
+++ src/layouts/helpers.test.tsx
@@ -1,8 +1,10 @@
+import cheerio from 'cheerio';
 import { shallow } from 'enzyme';
 import React from 'react';
 
 import { GIBIBYTE, KIBIBYTE, MEBIBYTE, TEBIBYTE } from './constants';
 import {
+  Abbreviation,
   bytesToHuman,
   capitalize,
   conditionallyDisplay,
@@ -23,19 +25,20 @@ describe(bytesToHuman, () => {
     const mebibytes = <span>{bytesToHuman(5.3 * MEBIBYTE)}</span>;
     const gibibytes = <span>{bytesToHuman(5.3 * GIBIBYTE)}</span>;
     const tebibytes = <span>{bytesToHuman(5.3 * TEBIBYTE)}</span>;
-
-    expect(shallow(bytes).html()).toContain('5 <abbr title="bytes">B</abbr>');
+    expect(shallow(bytes).html()).toContain(
+      '5 <abbr role="tooltip" tabindex="0" data-module="tooltip" aria-label="bytes">B</abbr>',
+    );
     expect(shallow(kibibytes).html()).toContain(
-      '5.30 <abbr title="kibibytes">KiB</abbr>',
+      '5.30 <abbr role="tooltip" tabindex="0" data-module="tooltip" aria-label="kibibytes">KiB</abbr>',
     );
     expect(shallow(mebibytes).html()).toContain(
-      '5.30 <abbr title="mebibytes">MiB</abbr>',
+      '5.30 <abbr role="tooltip" tabindex="0" data-module="tooltip" aria-label="mebibytes">MiB</abbr>',
     );
     expect(shallow(gibibytes).html()).toContain(
-      '5.30 <abbr title="gibibytes">GiB</abbr>',
+      '5.30 <abbr role="tooltip" tabindex="0" data-module="tooltip" aria-label="gibibytes">GiB</abbr>',
     );
     expect(shallow(tebibytes).html()).toContain(
-      '5.30 <abbr title="tebibytes">TiB</abbr>',
+      '5.30 <abbr role="tooltip" tabindex="0" data-module="tooltip" aria-label="tebibytes">TiB</abbr>',
     );
   });
 });
@@ -53,5 +56,16 @@ describe(conditionallyDisplay, () => {
 describe(capitalize, () => {
   it('should do the right thing', () => {
     expect(capitalize('test')).toEqual('Test');
+  });
+});
+
+describe(Abbreviation, () => {
+  it('it should render the abbreviation tooltip correctly', () => {
+    const output = <Abbreviation description="For the win">FTW</Abbreviation>;
+    const $ = cheerio.load(shallow(output).html());
+    expect($('abbr').attr('tabindex')).toBe('0');
+    expect($('abbr').text()).toBe('FTW');
+    expect($('abbr').attr('aria-label')).toBe('For the win');
+    expect($('abbr').attr('role')).toBe('tooltip');
   });
 });
