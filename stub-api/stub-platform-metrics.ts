400805a8b72cf24e0cfb9efe4fa48f190b2582a9
kr8n3r <jani.kraner@digital.cabinet-office.gov.uk>
2021-03-04 16:58:03 +0000

    Stub data api for /performance page
    Page expects data to come from a `metrics.json` file. In staging and
    prod this is served by the  paas-admin-data` internal app, but for
    local development there was none.
    
    This introduces an endpoint as part of the `stub-apis-env` script on
    `npm run start:stub-api` command.
    
    Data is pseudo-randomly generated for the last year in weekly
    increments, much like the real data.

---
+++ stub-api/stub-platform-metrics.ts
@@ -0,0 +1,54 @@
+import express from 'express';
+
+import { IStubServerPorts } from './index';
+
+function mockPlatformMetrics(
+  app: express.Application,
+  _config: IStubServerPorts,
+): express.Application {
+
+  app.get('/metrics.json', (_req, res) => {
+
+    const aYearAgo = new Date(new Date().setFullYear(new Date().getFullYear() - 1));
+
+    const randomValue = (min: number, max: number) => {
+      return Math.random() * (max - min) + min;
+    };
+
+    const generateData = (startValue: number, endValue: number) => {
+      const arrayOfData = [];
+      for (let i = 0; i < 52; i++) {
+        arrayOfData.push({ date: new Date(aYearAgo).setDate(aYearAgo.getDate() + (i*7)), value: Math.floor(randomValue(startValue, endValue)) });
+      }
+
+return arrayOfData;
+    };
+    const response = {
+      'applications': [
+        {
+          'metrics': generateData( 1000,2000),
+        },
+      ],
+      'services': [
+        {
+          'metrics': generateData(100,800),
+        },
+      ],
+      'organizations': [
+        {
+          'metrics': generateData(50,100),
+        },
+        {
+          'metrics': generateData(10, 30),
+        },
+      ],
+      'uptime': randomValue(99,100).toFixed(2),
+    };
+    res.send(JSON.stringify(response));
+  },
+);
+
+  return app;
+}
+
+export default mockPlatformMetrics;
