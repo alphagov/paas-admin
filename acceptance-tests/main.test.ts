3c7b7604a712903c2a6972770988afdc2d8ae181
Tom Rosier <tom.rosier@digital.cabinet-office.gov.uk>
2021-07-05 10:40:57 +0100

    WIP

--- acceptance-tests/main.test.ts
+++ acceptance-tests/main.test.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import expect from 'expect';
-import { after,before,describe,it } from 'mocha';
 import pino from 'pino';
-import Browser from 'zombie';
+import puppeteer from 'puppeteer';
+import puppeteer_expect from 'expect-puppeteer'
 
 import { AccountsClient } from '../src/lib/accounts';
 import CloudFoundryClient from '../src/lib/cf/cf';
@@ -23,39 +23,39 @@ if (!ACCOUNTS_PASSWORD) { throw 'ACCOUNTS_PASSWORD environment variable not set'
 if (!ADMIN_USERNAME) { throw 'ADMIN_USERNAME environment variable not set'; }
 if (!ADMIN_PASSWORD) { throw 'ADMIN_PASSWORD environment variable not set'; }
 
-describe('paas-admin', function () {
-  this.timeout(10000);
+describe('paas-admin', () => {
+  // // PASSING
+  // describe('when the client is not authenticated', () => {
+  //   it('passes its healthcheck', async () => {
+  //     const response = await axios.request({ url: `${PAAS_ADMIN_BASE_URL  }/healthcheck` });
 
-  describe('when the client is not authenticated', () => {
-    it('passes its healthcheck', async () => {
-      const response = await axios.request({ url: `${PAAS_ADMIN_BASE_URL  }/healthcheck` });
+  //     expect(response.status).toEqual(200);
+  //     expect(response.data).toEqual({ message:'OK' });
+  //   });
 
-      expect(response.status).toEqual(200);
-      expect(response.data).toEqual({ message:'OK' });
-    });
-
-    it('redirects to the login service', async () => {
-      try {
-        await axios.request({ url: PAAS_ADMIN_BASE_URL, maxRedirects: 0 });
-      }
-      catch(rejection) {
-        expect(rejection.response.status).toEqual(302);
-      }
-    });
-  });
+  //   it('redirects to the login service', async () => {
+  //     try {
+  //       await axios.request({ url: PAAS_ADMIN_BASE_URL, maxRedirects: 0 });
+  //     }
+  //     catch(rejection) {
+  //       expect(rejection.response.status).toEqual(302);
+  //     }
+  //   });
+  // });
 
   describe('when the client is authenticated', () => {
     const randomSuffix = `${  Math.floor(Math.random()*1e6)}`;
     const managerUserEmail = `CAT-paas-admin-acceptance-manager-${randomSuffix}@example.com`;
     const managerUserPassword = `${Math.floor(Math.random()*1e12)}`;
 
-    const browser = new Browser();
+    const browser = puppeteer.launch({ headless: false })
+    var page: puppeteer.Page;
 
     let cfClient: CloudFoundryClient;
     let uaaClient: UAAClient;
     let managerUserGuid: string;
 
-    before(async () => {
+    beforeAll(async () => {
       cfClient = new CloudFoundryClient({
         apiEndpoint: CF_API_BASE_URL,
         logger: pino({ level: 'silent' }),
@@ -86,68 +86,72 @@ describe('paas-admin', function () {
       managerUserGuid = uaaUser.id;
     });
 
-    after(async () => {
+    afterAll(async () => {
       if (managerUserGuid) {
         await uaaClient.deleteUser(managerUserGuid);
         await cfClient.deleteUser(managerUserGuid);
       }
+      (await browser).close();
     });
 
-    before(async () => {
-      await <any>browser.visit(PAAS_ADMIN_BASE_URL);
-      browser.assert.text('button', 'Continue with email and password sign in');
+    beforeAll(async (done) => {
+      page = await (await browser).newPage();
 
-      await <any>browser.fill('username', managerUserEmail);
-      await <any>browser.fill('password', managerUserPassword);
-      await <any>browser.pressButton('Continue  with email and password sign in'); // two spaces important because of DOM textContent
-      browser.assert.text('a', /Sign out/);
-    });
+      await page.goto(PAAS_ADMIN_BASE_URL);
 
-    it('should show a count of orgs on the home page', async () => {
-      await <any>browser.visit(PAAS_ADMIN_BASE_URL);
-      browser.assert.text('p', /There are 0 organisations/);
-    });
+      await puppeteer_expect(page).toFill('input[name=username]', managerUserEmail)
+      await puppeteer_expect(page).toFill('input[name=password]', managerUserPassword)
+      await puppeteer_expect(page).toClick('button', {text: /Continue/})
+      await puppeteer_expect(page).toMatchElement('a',{ text: /Sign out/ })
+      done();
+    }, 30000);
 
-    describe('when the user is an organisation manager', () => {
-      let orgGuid: string;
-      let developerUserGuid: string;
-      const orgName = `CAT-paas-admin-${randomSuffix}`;
-      const developerUserEmail = `CAT-paas-admin-acceptance-developer-${randomSuffix}@example.com`;
+    it('should show a count of orgs on the home page', async (done) => {
+      await page.goto(PAAS_ADMIN_BASE_URL);
+      await puppeteer_expect(page).toMatchElement('p', { text: /There are 0 organisations/ });
+      done();
+    }, 30000);
 
-      before(async () => {
-        const quotaDefinitions = await cfClient.quotaDefinitions({ name: 'small' });
-        const quotaGuid = quotaDefinitions[0].metadata.guid;
-        const organisation = await cfClient.createOrganization({
-          name: orgName,
-          quota_definition_guid: quotaGuid,
-        });
-        orgGuid = organisation.metadata.guid;
-        await cfClient.setOrganizationRole(orgGuid, managerUserGuid, 'managers', true);
+    // describe('when the user is an organisation manager', () => {
+    //   let orgGuid: string;
+    //   let developerUserGuid: string;
+    //   const orgName = `CAT-paas-admin-${randomSuffix}`;
+    //   const developerUserEmail = `CAT-paas-admin-acceptance-developer-${randomSuffix}@example.com`;
 
-        const uaaUser = await uaaClient.createUser(developerUserEmail, `${Math.floor(Math.random() * 1e12)}`);
-        await cfClient.createUser(uaaUser.id);
-        developerUserGuid = uaaUser.id;
-      });
+    //   beforeAll(async () => {
+    //     const quotaDefinitions = await cfClient.quotaDefinitions({ name: 'small' });
+    //     const quotaGuid = quotaDefinitions[0].metadata.guid;
+    //     const organisation = await cfClient.createOrganization({
+    //       name: orgName,
+    //       quota_definition_guid: quotaGuid,
+    //     });
+    //     orgGuid = organisation.metadata.guid;
+    //     await cfClient.setOrganizationRole(orgGuid, managerUserGuid, 'managers', true);
 
-      after(async () => {
-        if (orgGuid) { await cfClient.deleteOrganization({ guid: orgGuid, recursive: true, async: false }); }
-        if (developerUserGuid) { await cfClient.deleteUser(developerUserGuid); }
-      });
+    //     const uaaUser = await uaaClient.createUser(developerUserEmail, `${Math.floor(Math.random() * 1e12)}`);
+    //     await cfClient.createUser(uaaUser.id);
+    //     developerUserGuid = uaaUser.id;
+    //   });
 
-      it('should show a count of orgs on the home page', async () => {
-        await <any>browser.visit(PAAS_ADMIN_BASE_URL);
-        browser.assert.text('p', /There is 1 organisation/);
-      });
+    //   afterAll(async () => {
+    //     if (orgGuid) { await cfClient.deleteOrganization({ guid: orgGuid, recursive: true, async: false }); }
+    //     if (developerUserGuid) { await cfClient.deleteUser(developerUserGuid); }
+    //   });
 
-      it('should invite a user', async () => {
-        await <any>browser.visit(`${PAAS_ADMIN_BASE_URL}/organisations/${orgGuid}/users/invite`);
-        browser.assert.text('h1', /Invite a new team member/);
-        await <any>browser.fill('email', developerUserEmail);
-        await <any>browser.check(`org_roles[${orgGuid}][managers][desired]`);
-        await <any>browser.pressButton('Send invitation');
-        browser.assert.text('.govuk-panel__title', /New team member successfully invited/);
-      });
-    });
+    //   it('should show a count of orgs on the home page', async () => {
+    //     await <any>browser.visit(PAAS_ADMIN_BASE_URL);
+    //     browser.assert.text('p', /There is 1 organisation/);
+    //   });
+
+    //   it('should invite a user', async () => {
+    //     await <any>browser.visit(`${PAAS_ADMIN_BASE_URL}/organisations/${orgGuid}/users/invite`);
+    //     browser.assert.text('h1', /Invite a new team member/);
+    //     await <any>browser.fill('email', developerUserEmail);
+    //     await <any>browser.check(`org_roles[${orgGuid}][managers][desired]`);
+    //     await <any>browser.pressButton('Send invitation');
+    //     browser.assert.text('.govuk-panel__title', /New team member successfully invited/);
+    //   });
+    // });
 
   });
 });
