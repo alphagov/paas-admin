4598259dca2a0497a8bcbe2989b6e482d671f571
Tristram Oaten <tris@oat.sh>
2021-08-04 15:43:09 +0100

    removed extraneous 'RDS'

--- src/components/marketplace/views.tsx
+++ src/components/marketplace/views.tsx
@@ -181,7 +181,7 @@ function documentationTitle(service: string, url: string): string {
           case u.pathname.toLowerCase().includes('elasticache'):
             return `AWS ElastiCache ${service} documentation`;
         }
-      return `AWS RDS ${service} documentation`;
+      return `AWS ${service} documentation`;
     case 'help.aiven.io':
       return `Aiven ${service} documentation`;
     default:
