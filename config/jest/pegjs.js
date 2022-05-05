117a02150932c27691db2216cd26e9d9479b0e19
kr8n3r <jani@kraner.dev>
2022-05-05 07:39:47 +0100

    Update Jest Transformers
    As per https://jestjs.io/docs/upgrading-to-jest28#transformer

--- config/jest/pegjs.js
+++ config/jest/pegjs.js
@@ -1,5 +1,7 @@
 module.exports = {
   process(src) {
-    return `const peg = require("pegjs");module.exports = peg.generate(${JSON.stringify(src)});`;
+    return {
+      code: `const peg = require("pegjs");module.exports = peg.generate(${JSON.stringify(src)});`,
+    };
   },
 };
