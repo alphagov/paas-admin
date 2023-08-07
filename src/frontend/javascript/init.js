afac7cc0cde85ae452b73a98a14b9ddb859be205
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2023-08-07 15:31:28 +0100

    Remove leftover cookie-code
    We remove cookie setting a while ago, but left a cleanup function to delete any stored for repeating visitors.
    
    We can now also delete that an all associated cookie function code.

--- src/frontend/javascript/init.js
+++ src/frontend/javascript/init.js
@@ -9,11 +9,6 @@ import {
 } from 'govuk-frontend'
 
 import Tooltip from './tooltip';
-import Cookies from './cookie-functions'
-
-var cookies = new Cookies()
-
-cookies.cookieCleanup()
 
 // there is ever only one header per page
 var $header = document.querySelector('[data-module="govuk-header"]')
