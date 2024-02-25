3434fb9912bccc491caff8895ddab0dacd1cf427
Jani Kraner <jani.kraner@digital.cabinet-office.gov.uk>
2024-02-25 20:38:25 +0000

    Add Vitest config and set-up files

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line import/no-unresolved
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'vitest.setup.ts',
+  },
+});
